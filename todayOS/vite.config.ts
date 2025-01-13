959c3afdca3e9595fad5a2b1e604bfe373c98c88
Tom Kwok <junhaotom@gmail.com>
2025-01-13 16:30:46 +1100

    Add Vite PWA compiler

--- todayOS/vite.config.ts
+++ todayOS/vite.config.ts
@@ -1,10 +1,41 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import path from "path"
+import { PWAAssetsOptions, VitePWA } from "vite-plugin-pwa"
+
+const pwaAssets: PWAAssetsOptions = {
+	config: true,
+	overrideManifestIcons: true,
+}
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		VitePWA({
+			mode: "development",
+			base: "/",
+			injectRegister: "inline",
+			strategies: "generateSW",
+			registerType: "autoUpdate",
+			includeAssets: ["favicon/favicon.svg"],
+			manifest: {
+				name: "GYST",
+				short_name: "GYST",
+				theme_color: "#f6f5ec",
+			},
+			pwaAssets,
+			devOptions: {
+				enabled: true,
+			},
+			workbox: {
+				globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
+				cleanupOutdatedCaches: true,
+				clientsClaim: true,
+				skipWaiting: true,
+			},
+		}),
+	],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
