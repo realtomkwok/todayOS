ded45e73cd60d914213acb9c8bee59b64502244c
Tom Kwok <junhaotom@gmail.com>
2025-03-19 21:24:09 +1100

    Fix #1; add configs for prettier

--- todayOS/src/layout/Layout.tsx
+++ todayOS/src/layout/Layout.tsx
@@ -1,12 +1,9 @@
-import React from "react"
+import React from "react";
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
 	return (
-		<div
-			className="w-full h-full font-sans text-base font-regular antialiased text-md-on-surface overflow-hidden fixed"
-			data-oid="34h89is"
-		>
+		<div className="w-full h-full font-sans text-base font-regular antialiased text-md-on-surface overflow-hidden fixed bg-md-surface">
 			{children}
 		</div>
-	)
-}
+	);
+};
